306f29427aed5980967f3d8b7cae63dec8ec9d5e
Serhii <serhii.poklonskyi@outlook.de>
2022-11-07 22:13:49 +0100

    igni package refactoring
    massive refactoring of the package structure, and refactor of mdb2fbx code

--- igni/mdb2fbx.py
+++ igni/mdb2fbx.py
@@ -2,81 +2,92 @@ from mdb import Mdb
 import fbx
 import sys
 
-sys.path.append("E:\\projects\\the_witcher\\content_pipeline\\igni\\utils\\")
-from fbxu import FBX
+
+MEMORY_MANAGER = fbx.FbxManager.Create()
 
 
-class _MdbFbxBridge:
+class OnModuleClose:
 
-    @classmethod
-    def node(cls,
-             mdb_node: Mdb.Node,
-             fbx_parent_node: fbx.FbxNode = None) -> fbx.FbxNode:
-        # TODO translation, rotation, and others
-        fbx_node: fbx.FbxNode = FBX.node()
-        if fbx_parent_node is not None:
-            fbx_parent_node.AddChild(fbx_node)
-        return fbx_node
+    def __del__(self):
+        print('on module close called')
+        MEMORY_MANAGER.Destroy()
 
-    @classmethod
-    def transfer_trimesh_data(cls,
-                              mdb_source: Mdb.Node,
-                              fbx_dest: fbx.FbxNode):
-        # TODO in the below both parts are always evaluated?
-        mesh: fbx.FbxMesh = fbx_dest.GetMesh() \
-            if fbx_dest.GetMesh() is not None \
-            else FBX.add_mesh(fbx_dest)
 
-        # source data
-        mdb_verts = mdb_source.node_data.mesh_data.vertices.data
-        mdb_faces = mdb_source.node_data.mesh_data.faces_array_pointer.data
+MODULE_CLOSE_INTERCEPTOR = OnModuleClose()
 
-        # vertices
-        mesh.InitControlPoints(len(mdb_verts))
-        for i in range(0, len(mdb_verts)):
-            vertex = mdb_verts[i]
-            mesh.SetControlPointAt(
-                fbx.FbxVector4(vertex.x, vertex.y, vertex.z, 0.0),
-                i
+
+def _build_fbx_mesh(fbx_mesh: fbx.FbxMesh, vertices, faces, normals, uvs):
+
+    fbx_mesh.InitControlPoints(len(vertices))
+    for i in range(0, len(vertices)):
+        vertex = vertices[i]
+        fbx_mesh.SetControlPointAt(
+            fbx.FbxVector4(vertex[0], vertex[1], vertex[2], 0.0),
+            i
+        )
+
+    for i in range(len(faces)):
+        face = faces[i]
+        fbx_mesh.BeginPolygon()
+        for vertex_index in face:
+            fbx_mesh.AddPolygon(vertex_index)
+        fbx_mesh.EndPolygon()
+
+    for uv_set in uvs:
+        uv_element: fbx.FbxLayerElementUV = fbx_mesh.CreateElementUV(uv_set)
+        uv_element.SetMappingMode(fbx.FbxLayerElementUV.eByControlPoint)
+        uv_element.SetReferenceMode(fbx.FbxLayerElement.eDirect)
+
+        for uv_coord in uvs[uv_set]:
+            uv_element.GetDirectArray().Add(fbx.FbxVector2(uv_coord[0], uv_coord[1]))
+
+
+def _build_fbx_node(fbx_node: fbx.FbxNode, source_node: Mdb.Node, fbx_scene: fbx.FbxScene):
+
+    fbx_node.SetName(source_node.node_name.string)
+
+    # do translation, rotation, etc.
+    if source_node.node_type == Mdb.NodeType.trimesh or \
+            source_node.node_type == Mdb.NodeType.skin:
+        mesh = fbx.FbxMesh.Create(fbx_scene, '')
+        fbx_node.AddNodeAttribute(mesh)
+        _build_fbx_mesh(mesh,
+                        [[v.x, v.y, v.z] for v in source_node.node_data.vertices.data],
+                        [[f.vert1, f.vert2, f.vert3] for f in source_node.node_data.faces.data],
+                        [[n.x, n.y, n.z] for n in source_node.node_data.normals.data],
+                        {'uv_set_1':[[c.u, c.v] for c in source_node.node_data.uvs[0].data]})
+    else:
+        pass
+
+
+def _build_fbx_scene(fbx_scene: fbx.FbxScene, source: Mdb):
+
+    def recursive_add_nodes(source_nodes, under_parent: fbx.FbxNode):
+        for source_node in source_nodes:
+            fbx_node = fbx.FbxNode.Create(fbx_scene, '')
+            under_parent.AddChild(fbx_node)
+            _build_fbx_node(fbx_node, source_node, fbx_scene)
+
+            recursive_add_nodes(
+                [child_ptr.data for child_ptr in source_node.children.data],
+                fbx_node
             )
 
-        # faces
-        for i in range(0, len(mdb_faces)):
-            face = mdb_faces[i]
-            mesh.BeginPolygon()
-            mesh.AddPolygon(face.c1)
-            mesh.AddPolygon(face.c2)
-            mesh.AddPolygon(face.c3)
-            mesh.EndPolygon()
+    recursive_add_nodes([child_ptr.data for child_ptr in source.root_node.children.data],
+                        fbx_scene.GetRootNode())
 
-    @classmethod
-    def transfer_node_data(cls,
-                           mdb_source: Mdb.Node,
-                           fbx_dest: fbx.FbxNode):
-        if mdb_source.node_type == Mdb.NodeType.trimesh:
-            _MdbFbxBridge.transfer_trimesh_data(mdb_source, fbx_dest)
-        else:
-            print('cannot transfer data for the node of an unhandled type')
 
-    @classmethod
-    def scene(cls, mdb: Mdb) -> fbx.FbxScene:
-        fbx_scene: fbx.FbxScene = FBX.scene()
-
-        # transfer the nodes
-        def _recursive_convert_nodes_(mdb_node: Mdb.Node,
-                                      fbx_parent: fbx.FbxNode):
-            fbx_node = _MdbFbxBridge.node(mdb_node, fbx_parent)
-            _MdbFbxBridge.transfer_node_data(mdb_node, fbx_node)
-            [_recursive_convert_nodes_(node_ptr.data, fbx_node)
-             for node_ptr in mdb_node.children.data]
-
-        _recursive_convert_nodes_(mdb.root_node, fbx_scene.GetRootNode())
-
-        return fbx_scene
+def export(scene: fbx.FbxScene, dest):
+    fbx_exporter = fbx.FbxExporter.Create(MEMORY_MANAGER, '')
+    fbx_exporter.Initialize(dest, -1, MEMORY_MANAGER.GetIOSettings())
+    fbx_exporter.Export(scene)
+    fbx_exporter.Destroy()
 
 
 def from_mdb(mdb: Mdb) -> fbx.FbxScene:
-    return _MdbFbxBridge.scene(mdb)
+    fbx_scene = fbx.FbxScene.Create(MEMORY_MANAGER, mdb.root_node.node_name.string)
+    _build_fbx_scene(fbx_scene, mdb)
+    return fbx_scene
 
 
 def from_bytes(byte_stream) -> fbx.FbxScene:
@@ -89,7 +100,7 @@ def from_path(path) -> fbx.FbxScene:
 
 def convert(source, dest):
     scene = from_path(source)
-    FBX.export(scene, dest)
+    export(scene, dest)
     scene.Destroy()
 
 
